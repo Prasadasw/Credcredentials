fa87f5625c5c27862b1a8cf5890a1f6022ae2b8a
Prasadasw <prasadaswar6870@gmail.com>
2026-08-10 15:11:47 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://dtgcred-be.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
